815838c083439e09569604dfc10d57185682b24d
Mark Rendle <mark@rendlelabs.com>
2022-05-12 10:28:35 +0100

    IngredientsImpl

---
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using Ingredients.Protos;
+
+namespace Ingredients.Services;
+
+public class IngredientsImpl : IngredientsService.IngredientsServiceBase
+{
+    private readonly ILogger<IngredientsImpl> _logger;
+
+    public IngredientsImpl(ILogger<IngredientsImpl> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+    {
+        return base.GetToppings(request, context);
+    }
+}
