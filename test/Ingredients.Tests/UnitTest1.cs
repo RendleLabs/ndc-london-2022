83cdef911359dbcbe32ad10000fc5091a82d7726
Mark Rendle <mark@rendlelabs.com>
2022-05-12 11:56:03 +0100

    Testing WIP

---
+++ test/Ingredients.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Ingredients.Protos;
+using Xunit;
+
+namespace Ingredients.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Topping x = null;
+    }
+}
